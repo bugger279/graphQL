d4c83957c102b56da464adc527a27cc9ac12809b
bugger279 <bugger279@gamil.com>
2020-10-28 18:58:52 +0530

    Class Validator added

---
+++ src/module/user/Register.ts
@@ -0,0 +1,27 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import bcrypt from "bcryptjs";
+import { User } from "../../entity/User";
+import { RegisterInput } from "./register/RegisterInput";
+
+@Resolver()
+export class RegisterResolver {
+  @Query(() => String, { nullable: true })
+  async hello() {
+    return "Bye";
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("data") {firstName,lastName,email,password} : RegisterInput
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    }).save();
+
+    return user;
+  }
+}
